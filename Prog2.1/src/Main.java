c4f22133cf4a4e7f6008775e3f222621162ddda1
Adrian Hirt <aedu.hirt@gmail.com>
2017-03-20 22:02:08 +0100

    started with new idea

--- Prog2.1/src/Main.java
+++ Prog2.1/src/Main.java
@@ -22,8 +22,49 @@ class Main {
                 continue;
             }
 
+            HashMap<String, Node> map = new HashMap<String, Node>();
 
+            for(int k = 0; k < numberOfPapers; k++) {
+                String next = input.next();
 
+                String first = next.substring(0, 2);
+                String second = next.substring(1, 3);
+
+                Node firstNode = null;
+                Node secondNode = null;
+
+                if(map.containsKey(first)) {
+                    firstNode = map.get(first);
+                }
+                else {
+                    firstNode = new Node(0,0);
+                    map.put(first, firstNode);
+                }
+                if(map.containsKey(second)) {
+                    secondNode = map.get(second);
+                }
+                else {
+                    secondNode = new Node(0,0);
+                    map.put(second, secondNode);
+                }
+                firstNode.outDeg += 1;
+                secondNode.inDeg += 1;
+            }
+
+            // TODO: Check if graph is euler graph
         }
     }
+}
+
+
+class Node {
+
+    int inDeg;
+    int outDeg;
+
+    public Node(int newIn, int newOut) {
+        inDeg = newIn;
+        outDeg = newOut;
+    }
+
 }
