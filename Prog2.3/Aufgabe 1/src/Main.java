3ed0ccc2defa06ab03126a63ff371610c4b683a1
Adrian Hirt <aedu.hirt@gmail.com>
2017-04-03 09:04:29 +0200

    Added template for 2.3 a)

---
+++ Prog2.3/Aufgabe 1/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int testCases = input.nextInt();
+
+        for(int i = 0; i < testCases; i++) {
+            int numberOfBears = input.nextInt();
+            int numberOfHunters = input.nextInt();
+            int numberOfNinjas = input.nextInt();
+
+            // bear kills ninja, ninja kills hunter, hunter kills bear
+        }
+    }
+}
