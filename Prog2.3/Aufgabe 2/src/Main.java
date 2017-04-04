3c44014ff8d3fbdf530bb12d5c572fa67dd2728b
Adrian Hirt <aedu.hirt@gmail.com>
2017-04-04 20:34:24 +0200

    Prepared Aufgabe 2

---
+++ Prog2.3/Aufgabe 2/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int testCases = input.nextInt();
+
+        for(int i = 0; i < testCases; i++) {
+            int slots = input.nextInt();
+            int spins = input.nextInt();
+            int desiredWins = input.nextInt();
+        }
+    }
+}
