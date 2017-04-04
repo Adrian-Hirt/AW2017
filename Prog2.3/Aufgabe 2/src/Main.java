2d98da7e47363d2a7fd8ed5ebe042a1f324331ed
Adrian Hirt <aedu.hirt@gmail.com>
2017-04-04 21:37:18 +0200

    Started aufgabe 2

--- Prog2.3/Aufgabe 2/src/Main.java
+++ Prog2.3/Aufgabe 2/src/Main.java
@@ -7,10 +7,37 @@ class Main {
 
         int testCases = input.nextInt();
 
-        for(int i = 0; i < testCases; i++) {
-            int slots = input.nextInt();
-            int spins = input.nextInt();
-            int desiredWins = input.nextInt();
+        for(int x = 0; x < testCases; x++) {
+            int slots = input.nextInt(); // n
+            int spins = input.nextInt(); // m
+            int desiredWins = input.nextInt(); // k
+
+            int zeroes = 0;
+            int other = 0;
+
+            for(int y = 0; y < slots; y++) {
+                int current = input.nextInt();
+                if(current == 0) {
+                    zeroes += 1;
+                }
+                else {
+                    other += 1;
+                }
+            }
+
+            double dp[][] = new double[spins][spins];
+
+            double p = ((double) zeroes) / ((double) slots);
+
+
+
+            for(int i = 0; i < spins; i++) {
+                for(int j = spins; j > 0; j--) {
+                    dp[i][j] = 0.0;
+                }
+            }
+
+
         }
     }
 }
