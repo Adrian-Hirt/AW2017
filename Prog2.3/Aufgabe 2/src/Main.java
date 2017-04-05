1c021e7f2a794d1944753764c99d4ae8932c891d
Adrian Hirt <aedu.hirt@gmail.com>
2017-04-05 08:05:28 +0200

    Aufgabe 2 done

--- Prog2.3/Aufgabe 2/src/Main.java
+++ Prog2.3/Aufgabe 2/src/Main.java
@@ -1,3 +1,5 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.*;
 
 class Main {
@@ -25,19 +27,45 @@ class Main {
                 }
             }
 
-            double dp[][] = new double[spins][spins];
+            double dp[][] = new double[101][101];
 
-            double p = ((double) zeroes) / ((double) slots);
-
-
-
-            for(int i = 0; i < spins; i++) {
-                for(int j = spins; j > 0; j--) {
-                    dp[i][j] = 0.0;
+            for(int i = 0; i < 101; i++) {
+                for(int j = 0; j < 101; j++) {
+                    dp[i][j] = -1.0;
                 }
             }
 
 
+            double p = ((double) zeroes) / ((double) slots);
+
+            double prob = calculate(0, spins, p, desiredWins, dp);
+
+            DecimalFormat df = new DecimalFormat("0.0######");
+            df.setRoundingMode(RoundingMode.HALF_DOWN);
+            System.out.println(df.format(prob));
+
         }
+    }
+
+    public static double calculate(int zeroes, int movesLeft, double p,
+                                   int desiredWins, double[][] dp) {
+
+        if(zeroes >= desiredWins) {
+            return 1.0;
+        }
+        if(desiredWins - zeroes > movesLeft) {
+            return 0.0;
+        }
+
+        if(dp[zeroes][movesLeft] != -1.0) {
+            return dp[zeroes][movesLeft];
+        }
+
+        double first = p * calculate(zeroes + 1, movesLeft - 1, p, desiredWins, dp);
+        double second = (1.0 - p) * calculate(0, movesLeft - 1, p, desiredWins, dp);
+
+        dp[zeroes][movesLeft] = first + second;
+
+        return first + second;
     }
 }
